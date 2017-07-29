5fdb90d21d7bc006495e1ebcb58ab2fdad652db7
Zlobrynya <zlobrynya@gmail.com>
2017-07-30 01:23:01 +0300

    Создал класс приема данных с устройства.

--- server/Server/src/server/Server.java
+++ server/Server/src/server/Server.java
@@ -18,30 +18,31 @@ import java.util.logging.Logger;
  * @author Nikita
  */
 public class Server {
-    private static boolean bExit;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        bExit = false;
+        
         // TODO code application logic here
         int port = 6666;       
         try {
             ServerSocket ss = new ServerSocket(port); // создаем сокет сервера и привязываем его к вышеуказанному порту
             System.out.println("Waiting for a client...");
-            
-            while(!bExit){
-                Socket socket = ss.accept(); // заставляем сервер ждать подключений и выводим сообщение когда кто-то связался с сервером
+            Socket socket = ss.accept();
+            InputData inputData = new InputData(socket);
+
+            while(!inputData.getbExit()){
+                 // заставляем сервер ждать подключений и выводим сообщение когда кто-то связался с сервером
                 System.out.println("Got a client :) ... Finally, someone saw me through all the cover!");
                 System.out.println();
                 
-                readData(socket);
+                inputData.readData(socket);
                 
-                socket.close();
+                
                 System.out.println("Client disconect");
                 System.out.println();
             }
-            
+            socket.close();
             
         } catch (IOException ex) {
             Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
@@ -49,19 +50,6 @@ public class Server {
         
     }
     
-    private static void readData(Socket socket){
-        try {
-            BufferedReader in = null;
-            in  = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            String input;
-
-            while ((input = in.readLine()) != null) {
-                    System.out.println(input);
-            }
-            in.close();
-        } catch (IOException ex) {
-            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+    
     
 }
