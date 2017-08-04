4ec5fa1deb1c2f5a6d20b4f7ecdc6d36b90ca1d9
Zlobrynya <zlobrynya@gmail.com>
2017-08-04 12:34:19 +0300

    Добавлен класс проверки id устройства

--- server/Server/src/server/Server.java
+++ server/Server/src/server/Server.java
@@ -5,9 +5,7 @@
  */
 package server;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.logging.Level;
@@ -22,6 +20,7 @@ public class Server {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
         
         // TODO code application logic here
         int port = 6666;       
